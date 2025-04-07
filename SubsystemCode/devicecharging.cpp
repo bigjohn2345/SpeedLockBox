996a86d470f008e093b764f3946fd39ef3e1e037
henrywilson4 <henrydw04@gmail.com>
2025-04-07 16:03:53 -0500

    Update devicecharging.cpp

--- SubsystemCode/devicecharging.cpp
+++ SubsystemCode/devicecharging.cpp
@@ -1,6 +1,7 @@
 void setup() {
   
-  pinMode(13, OUTPUT)
+  pinMode(13, OUTPUT);
+  bool devicecharging=FALSE;
 
 }
 
@@ -9,12 +10,14 @@ void loop() {
   if(doorlatched) {    // check that door is latched
 
     digitalWrite(13, HIGH);
+    devicecharging=TRUE;
 
   }
 
   else if(!doorlatched) {
 
     digitalWrite(13, LOW);
+    devicecharging=FALSE;
 
   }
 
