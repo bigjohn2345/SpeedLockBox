0ef5714205c2ac5e99b07cdf630ba9c6e362c3ad
bigjohn2345 <barrettjerry6@gmail.com>
2025-04-07 15:57:15 -0500

    Update combined_code.c

--- combined_code.c
+++ combined_code.c
@@ -1,7 +1,65 @@
-void setup() {
+//--Ben Barrett 2025--//
+//--GPS Subsystem Code: Sets bool "IsAtSpeed" true when above x mph speed--//
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
 
+#include <ArduinoNmeaParser.h>
+
+/**************************************************************************************
+ * FUNCTION DECLARATION
+ **************************************************************************************/
+
+void onRmcUpdate(nmea::RmcData const);
+void onGgaUpdate(nmea::GgaData const);
+
+/**************************************************************************************
+ * GLOBAL VARIABLES
+ **************************************************************************************/
+
+ArduinoNmeaParser parser(onRmcUpdate, onGgaUpdate);
+bool IsAtSpeed=false;
+
+/**************************************************************************************
+ * SETUP/LOOP
+ **************************************************************************************/
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial1.begin(9600);
 }
 
-void loop() {
+void loop()
+{
+  while (Serial1.available()) {
+    parser.encode((char)Serial1.read());
+  }
+}
 
+/**************************************************************************************
+ * FUNCTION DEFINITION
+ **************************************************************************************/
+
+void onRmcUpdate(nmea::RmcData const rmc)
+{
+  if (rmc.is_valid)
+  {
+    Serial.print(" Speed: ");
+    Serial.print(rmc.speed*2.23694);
+
+    if (((rmc.speed)*2.23694)>15) {
+      IsAtSpeed=true;
+      Serial.print(" AtSpeed");
+    } else {IsAtSpeed=false;}
+
+  } else {
+    Serial.print("No Fix");
+    IsAtSpeed=false;
+  }
+
+  Serial.println();
+}
+
+void onGgaUpdate(nmea::GgaData const gga) {
 }
