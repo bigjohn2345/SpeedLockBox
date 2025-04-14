19383a3774e8d0c926561b3a1c210c8a2f21115b
JacobEGood <jeg555@msstate.edu>
2025-04-14 16:31:49 -0500

    Update RFIDDetection.cpp

--- SubsystemCode/RFIDDetection.cpp
+++ SubsystemCode/RFIDDetection.cpp
@@ -35,43 +35,50 @@ void loop() {
 }
 //other test code
 
-#include <SPI.h>
-#include <MFRC522.h>
+ if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+    // Authorized UID
+    byte authorizedUID[] = { 0x63, 0x63, 0x07, 0x04 };
+    byte uidLength = sizeof(authorizedUID);
 
-#define SS_PIN 10
-#define RST_PIN 5
-
-MFRC522 rfid(SS_PIN, RST_PIN);
-bool phoneDetected = false;
-
-void setup() {
-  Serial.begin(9600);
-  SPI.begin(); 
-  rfid.PCD_Init(); 
-  Serial.println("Tap RFID/NFC Tag on reader");
-}
-
-void loop() {
-  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
-    if (!phoneDetected) {
-      phoneDetected = true;
-      Serial.print("Phone Detected: ");
-      for (int i = 0; i < rfid.uid.size; i++) {
-        Serial.print(rfid.uid.uidByte[i] < 0x10 ? "0" : "");
-        Serial.print(rfid.uid.uidByte[i], HEX);
-        if (i < rfid.uid.size - 1) Serial.print(":");
+    isAuthorized = (rfid.uid.size == uidLength);
+    for (int i = 0; i < uidLength && isAuthorized; i++) {
+      if (rfid.uid.uidByte[i] != authorizedUID[i]) {
+        isAuthorized = false;
       }
-      Serial.println();
     }
+
+    if (isAuthorized) {
+      Serial.print(" Authorized Tag Detected: ");
+    } else {
+      Serial.print("Unauthorized Tag: ");
+    }
+
+    // Print UID
+    for (int i = 0; i < rfid.uid.size; i++) {
+      Serial.print(rfid.uid.uidByte[i] < 0x10 ? "0" : "");
+      Serial.print(rfid.uid.uidByte[i], HEX);
+      if (i < rfid.uid.size - 1) Serial.print(":");
+    }
+    Serial.println();
 
     rfid.PICC_HaltA();
     rfid.PCD_StopCrypto1();
-  } 
-  else if (phoneDetected) {
-    // Card has been removed
-    phoneDetected = false;
-    Serial.println("Phone Removed.");
   }
 
-  delay(100); // Short delay for stability
-}
+  // Detect presence of card continuously (even if it's not new)
+  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+    // Same UID check again (to maintain lock while present)
+    byte authorizedUID[] = { 0x63, 0x63, 0x07, 0x04 };
+    byte uidLength = sizeof(authorizedUID);
+
+    bool cardStillPresent = (rfid.uid.size == uidLength);
+    for (int i = 0; i < uidLength && cardStillPresent; i++) {
+      if (rfid.uid.uidByte[i] != authorizedUID[i]) {
+        cardStillPresent = false;
+      }
+    }
+
+    doorLocked = cardStillPresent;
+  } else {
+    doorLocked = false;  // Card removed
+  }
